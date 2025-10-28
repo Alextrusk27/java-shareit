c1e9e719db8bb07f9fa4b2d24609cee27bab6551
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2025-10-28 22:14:56 +0300

    chore: add logging for user package

--- src/main/java/ru/practicum/shareit/user/controller/UserControllerImpl.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserControllerImpl.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.service.UserServiceImpl;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserControllerImpl implements UserController {
@@ -13,21 +15,25 @@ public class UserControllerImpl implements UserController {
 
     @Override
     public UserDto create(User user) {
+        log.info("POST request received: new user create");
         return userService.create(user);
     }
 
     @Override
     public UserDto update(User user, long id) {
+        log.info("PATCH request received: user ID:{} update", id);
         return userService.update(user, id);
     }
 
     @Override
     public void delete(long id) {
+        log.info("DELETE request received: user ID:{} delete", id);
         userService.delete(id);
     }
 
     @Override
     public UserDto findById(long id) {
+        log.info("GET request received: get user ID:{}", id);
         return userService.findById(id);
     }
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -8,6 +9,7 @@ import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.InMemoryUserRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -17,6 +19,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto create(User user) {
         userStorage.save(user);
+        log.info("User created: {}", user);
         return findById(user.getId());
     }
 
@@ -24,12 +27,14 @@ public class UserServiceImpl implements UserService {
     public UserDto update(User user, long id) {
         throwIfUserNotExists(id);
         userStorage.update(user, id);
+        log.info("User updated: {}", user);
         return findById(id);
     }
 
     @Override
     public void delete(long id) {
         throwIfUserNotExists(id);
+        log.info("User deleted: ID{}", id);
         userStorage.delete(id);
     }
 
@@ -37,6 +42,7 @@ public class UserServiceImpl implements UserService {
     public UserDto findById(long id) {
         User user = userStorage.findById(id).orElseThrow(() ->
                 new NotFoundException("User with ID %d not found".formatted(id)));
+        log.debug("User found: {}", user);
         return UserMapper.mapUserToDto(user);
     }
 
